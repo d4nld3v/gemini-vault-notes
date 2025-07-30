9e1b81d73fcfa14c4048bab9f71740337fbb8985
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-07-30 12:38:39 +0200

    Initial Commit

---
+++ gemini-vault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gemini-vault
+//
+//  Created by Daniel Enriquez Baena on 30/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gemini-vault/gemini_vaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemini_vaultApp.swift
+//  gemini-vault
+//
+//  Created by Daniel Enriquez Baena on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct gemini_vaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
